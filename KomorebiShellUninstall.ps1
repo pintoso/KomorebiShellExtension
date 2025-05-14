71baa5ace352dc899ed6e9327b2e2ff930c2d337
PINTOSO <otacopintoso@gmail.com>
2025-05-14 01:24:58 -0300

    Update KomorebiShellUninstall.ps1

--- KomorebiShellUninstall.ps1
+++ KomorebiShellUninstall.ps1
@@ -1,5 +1,14 @@
-$fileKeyPath = "Registry::HKEY_CURRENT_USER\Software\Classes\*\shell\komorebiignore"
-Remove-Item -Path $fileKeyPath -Recurse -Confirm
+$komorebiKeyPaths = @(
+    "Registry::HKEY_CURRENT_USER\Software\Classes\exefile\shell\Komorebi"     # .exe
+    # "Registry::HKEY_CURRENT_USER\Software\Classes\dllfile\shell\Komorebi"   # .dll
+    # "Registry::HKEY_CURRENT_USER\Software\Classes\SystemFileAssociations\.txt\shell\Komorebi"  # .txt
+)
 
-$fileKeyPath = "Registry::HKEY_CURRENT_USER\Software\Classes\*\shell\komorebimanage"
-Remove-Item -Path $fileKeyPath -Recurse -Confirm
+foreach ($key in $komorebiKeyPaths) {
+    if (Test-Path $key) {
+        Remove-Item -Path $key -Recurse -Force
+        Write-Host "Removed:" $key -ForegroundColor Green
+    } else {
+        Write-Host "Key not found:" $key -ForegroundColor Yellow
+    }
+}
